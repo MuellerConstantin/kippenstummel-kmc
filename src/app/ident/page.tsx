010898f4ffb622cd9acb4d47537958359c4ed2ee
Constantin Müller <info@mueller-constantin.de>
2025-06-01 21:47:04 +0200

    feat: add option for deleting identities

--- src/app/ident/page.tsx
+++ src/app/ident/page.tsx
@@ -1,16 +1,63 @@
 "use client";
 
+import { Button } from "@/components/atoms/Button";
+import { Modal } from "@/components/atoms/Modal";
+import { DeleteIdentDialog } from "@/components/organisms/ident/DeleteIdentDialog";
 import { IdentTable } from "@/components/organisms/ident/IdentTable";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import { DialogTrigger } from "react-aria-components";
 
 export default function Ident() {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [selected, setSelected] = useState<
+    | {
+        identity: string;
+        credibility: number;
+        issuedAt: string;
+        lastInteractionAt?: string;
+        averageInteractionInterval: number;
+        lastInteractionPosition?: { longitude: number; latitude: number };
+        unrealisticMovementCount: number;
+        voting: {
+          totalCount: number;
+          upvoteCount: number;
+          downvoteCount: number;
+        };
+        registrations: {
+          totalCount: number;
+        };
+      }[]
+    | null
+  >(null);
+
   return (
     <div className="flex grow flex-col">
       <div className="mx-auto flex w-full max-w-screen-2xl grow flex-col gap-4 p-4">
+        <div className="flex justify-start">
+          <Button
+            onPress={() => setShowDeleteDialog(true)}
+            isDisabled={!selected || selected.length === 0}
+          >
+            <div className="flex items-center gap-2">
+              <Trash className="h-4 w-4" />
+              <div>Delete</div>
+            </div>
+          </Button>
+          <DialogTrigger
+            isOpen={showDeleteDialog}
+            onOpenChange={setShowDeleteDialog}
+          >
+            <Modal>
+              <DeleteIdentDialog idents={selected || []} />
+            </Modal>
+          </DialogTrigger>
+        </div>
         <div className="space-y-4">
           <h4 className="text-lg font-semibold text-slate-900 dark:text-slate-100">
             Issued Identities
           </h4>
-          <IdentTable />
+          <IdentTable onSelect={setSelected} />
         </div>
       </div>
     </div>
---
+++ src/components/organisms/ident/DeleteIdentDialog.tsx
@@ -0,0 +1,98 @@
+import React, { useCallback, useState } from "react";
+import { DialogProps, Heading } from "react-aria-components";
+import { Dialog } from "@/components/atoms/Dialog";
+import { Button } from "@/components/atoms/Button";
+import useApi from "@/hooks/useApi";
+import { Spinner } from "@/components/atoms/Spinner";
+import { useSWRConfig } from "swr";
+
+interface DeleteIdentDialogProps extends Omit<DialogProps, "children"> {
+  idents: {
+    identity: string;
+    credibility: number;
+    issuedAt: string;
+    lastInteractionAt?: string;
+    averageInteractionInterval: number;
+    lastInteractionPosition?: { longitude: number; latitude: number };
+    unrealisticMovementCount: number;
+    voting: {
+      totalCount: number;
+      upvoteCount: number;
+      downvoteCount: number;
+    };
+    registrations: {
+      totalCount: number;
+    };
+  }[];
+}
+
+export function DeleteIdentDialog(props: DeleteIdentDialogProps) {
+  const { mutate } = useSWRConfig();
+  const { idents } = props;
+
+  const api = useApi();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const onDelete = useCallback(
+    (close: () => void) => {
+      setIsLoading(true);
+      setError(null);
+
+      Promise.all(
+        idents.map((ident) => api.delete(`/kmc/ident/${ident.identity}`)),
+      )
+        .then(() => {
+          mutate((key: string) => /^.*\/kmc\/ident.*$/.test(key), null);
+        })
+        .then(close)
+        .catch(() => setError("An unexpected error occurred, please retry!"))
+        .finally(() => {
+          setIsLoading(false);
+        });
+    },
+    [api, idents, mutate],
+  );
+
+  return (
+    <Dialog {...props}>
+      {({ close }) => (
+        <>
+          <Heading
+            slot="title"
+            className="my-0 text-xl leading-6 font-semibold"
+          >
+            {props.idents.length > 1 ? "Delete Identities" : "Delete Identity"}
+          </Heading>
+          <div className="mt-4 flex flex-col gap-4">
+            <div>
+              Are you sure you want to delete{" "}
+              {props.idents.length > 1
+                ? `${props.idents.length} idenities`
+                : "this identity"}
+              ? This action is irreversible.
+            </div>
+            {error && <p className="text-center text-red-500">{error}</p>}
+            <div className="flex justify-start gap-4">
+              <Button
+                variant="secondary"
+                onPress={() => close()}
+                className="w-full"
+              >
+                Cancel
+              </Button>
+              <Button
+                onPress={() => onDelete(close)}
+                className="flex w-full justify-center"
+              >
+                {!isLoading && <span>Confirm</span>}
+                {isLoading && <Spinner />}
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
+    </Dialog>
+  );
+}
