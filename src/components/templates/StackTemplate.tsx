a07937e86d5009f1fc5ccadca3ab76ce97813828
Constantin Müller <info@mueller-constantin.de>
2025-10-07 14:08:45 +0200

    feat: add modern message banner carousel

--- src/components/templates/StackTemplate.tsx
+++ src/components/templates/StackTemplate.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { Navbar } from "@/components/organisms/Navbar";
 import { Footer } from "@/components/molecules/Footer";
 import { useEnv } from "@/contexts/RuntimeConfigProvider";
-import TestSystemBanner from "../organisms/TestSystemBanner";
-import PreReleaseBanner from "../organisms/PreReleaseBanner";
+import { MessageBannerCarousel } from "../organisms/MessageBannerCarousel";
 
 export function StackTemplate({
   children,
@@ -15,12 +14,39 @@ export function StackTemplate({
   const isTestSystem = useEnv("NEXT_PUBLIC_IS_TEST_SYSTEM") === "true";
   const isPreRelease = useEnv("NEXT_PUBLIC_IS_PRE_RELEASE") === "true";
 
+  const messages = useMemo(() => {
+    const messages: {
+      title: string;
+      description: string;
+      variant?: "default" | "success" | "error" | "info" | "warning";
+    }[] = [];
+
+    if (isTestSystem) {
+      messages.push({
+        title: "Notice:",
+        description:
+          "This is a test environment. Any changes or inputs here will not affect the live system.",
+        variant: "warning",
+      });
+    }
+
+    if (isPreRelease) {
+      messages.push({
+        title: "Pre-Release:",
+        description:
+          "This is a not officially released version. Changes and errors may occur.",
+        variant: "warning",
+      });
+    }
+
+    return messages;
+  }, [isTestSystem, isPreRelease]);
+
   return (
     <div className="flex h-full min-h-screen flex-col">
       <header>
         <Navbar />
-        {isTestSystem && <TestSystemBanner />}
-        {isPreRelease && <PreReleaseBanner />}
+        <MessageBannerCarousel messages={messages} />
       </header>
       <main className="flex grow flex-col bg-white dark:bg-slate-800">
         {children}
